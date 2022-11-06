d8a0f8daf359eb19008cc86ca0922585c589071c
cristeacrs <cristea.cristi@yahoo.com>
2022-11-06 18:56:03 +0200

    [FEAT] Setup TS

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-const express = require("express");
+import express, { Request, Response } from "express";
 const app = express();
 const port = 8080; // default port to listen
 
 // define a route handler for the default home page
-app.get("/", (req, res) => {
+app.get("/", (req: Request, res: Response) => {
   res.send("Hello world!");
 });
 
